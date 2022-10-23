4b0f95cb93ae28b6233ccbd0e825b7e9256d2229
钱难有 <10794023+xxxxasd@user.noreply.gitee.com>
2022-10-23 22:13:39 +0800

    提交

--- main.cpp
+++ main.cpp
@@ -7,6 +7,29 @@
 using namespace std;
 
 int main() {
+    int a[3][4];
+    int sum = 0;
+    int b = 0, t;
 
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (j == 5) {
+                sum += a[i][j];
+                a[i][j] = sum;
+            } else if (j == 4) {
+                a[i][j] = b;
+            } else {
+                cin >> a[i][j];
+            }
+        }
+        sum = 0;
+    }
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << a[i][j] << " ";
+        }
+
+        cout << endl;
+    }
     return 0;
 }
